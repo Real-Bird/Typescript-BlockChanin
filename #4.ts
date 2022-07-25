6c202cd3f357fbe874419321b2fdc215ba111750
Real-Bird <marbil815@gmail.com>
2022-07-25 22:59:18 +0900

    22-07-25 #4

--- #4.ts
+++ #4.ts
@@ -30,3 +30,80 @@ dict.add(kimchi);
 dict.def("Kimchi");
 
 console.log(dict);
+
+// interface User {
+//   name: string;
+// }
+
+// interface Player extends User {}
+
+// const js: Player = {
+//   name: "js",
+//   nickname: "JS",
+//   health: 10,
+// };
+
+// interface User {
+//   name: string;
+// }
+// interface User {
+//   nickname: string;
+// }
+// interface User {
+//   health: number;
+// }
+
+interface User {
+  firstName: string;
+  lastName: string;
+  sayHi(name: string): string;
+  fullName(): string;
+}
+
+interface Human {
+  health: number;
+}
+
+class Player implements User, Human {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public health: number
+  ) {}
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  sayHi(name: string): string {
+    return `Hello ${name}. My name is ${this.fullName()}`;
+  }
+}
+
+// generic + class + interface
+
+interface SStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringStorage = new LocalStorage<string>();
+stringStorage.get("key");
+stringStorage.set("Hello", "1");
+
+const booleanStorage = new LocalStorage<boolean>();
+
+booleanStorage.get("SS");
